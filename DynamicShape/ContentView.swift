ae53e546a34bfa7eba552e73b4e8e903ff4bf9df
Russell Gordon <rgordon@lcs.on.ca>
2023-10-31 19:55:57 -0400

    Static triangle.

--- DynamicShape/ContentView.swift
+++ DynamicShape/ContentView.swift
@@ -7,15 +7,35 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        
+        return path
+        
+    }
+    
+}
+
 struct ContentView: View {
+    
     var body: some View {
+
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Triangle()
+                .fill(.black)
+                .aspectRatio(1.0, contentMode: .fit)
+                .padding()
         }
-        .padding()
+        
     }
 }
 
