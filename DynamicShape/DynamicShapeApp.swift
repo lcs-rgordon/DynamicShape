ef719de85f5833889825bb63f169baf7191f87d4
Russell Gordon <rgordon@lcs.on.ca>
2023-10-31 19:49:58 -0400

    Initial Commit

---
+++ DynamicShape/DynamicShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicShapeApp.swift
+//  DynamicShape
+//
+//  Created by Russell Gordon on 2023-10-31.
+//
+
+import SwiftUI
+
+@main
+struct DynamicShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
